f19bc787fa8a3ae96806210671f4a6a744409fcf
Damien Dumontet <damiendumontet@Mac-mini-de-Damien.home>
2021-01-21 18:26:34 +0100

    start portfolio

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,65 @@
 <script lang="ts">
-  console.log('Hello World!');
 </script>
 
 <main>
-  <h1>Hello World!</h1>
+  <section>
+    <h1>Damien <br /> Dumontet <br /> Creative Developer.</h1>
+    <div class="button">Click.</div>
+    <div class="img-container">
+      <img src="assets/portrait.png" alt="portrait" />
+    </div>
+  </section>
 </main>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    position: relative;
+    height: 100vh;
+    width: 100vw;
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+    h1 {
+      z-index: 10;
+      margin: 0 0 0 4vw;
+      font-family: 'Abril Fatface', cursive;
+      font-size: 8vw;
+      line-height: 18vh;
+      color: black;
+    }
+
+    .button {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: black;
+      color: white;
+      margin: 2vw 0 0 4vw;
+      font-size: 4vh;
+      height: 8vh;
+      width: 16vh;
+    }
+
+    .img-container {
+      display: flex;
+      align-items: flex-end;
+      position: absolute;
+      right: 16vw;
+      height: 70vh;
+      width: 25vw;
+      overflow: hidden;
+      background-color: #f5f5f5;
+
+      filter: grayscale(1);
+
+      img {
+        width: 150%;
+      }
     }
   }
 </style>
--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -3,5 +3,5 @@ import App from '../App.svelte';
 
 test('should render', () => {
   const result = render(App);
-  expect(() => result.getByText('Hello World!')).not.toThrow();
+  expect(() => result.component).not.toThrow();
 });
