aae901c8cef77f441c9dc414e7c77345f49c2412
Damien Dumontet <damien.dumontet@protonmail.com>
2022-11-13 15:41:11 -0300

    setup navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,55 @@
 import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const location = useLocation();
+
+  console.log(location);
+
   return (
     <div className="flex align-center justify-between w-full pt-6">
       <div className="h-10"></div>
       <div className="flex ring-1 ring-zinc-100 rounded-full px-6 py-3 shadow-md text-sm text-zinc-800 font-medium">
-        <p className="mr-6 cursor-pointer hover:text-teal-400 text-teal-400">
+        <a
+          href="/"
+          className={`mr-6 cursor-pointer hover:text-teal-400 ${
+            location.pathname === "/" ? "text-teal-400" : ""
+          }`}
+        >
           Hey!
-        </p>
-        <p className="mr-6 cursor-pointer hover:text-teal-400 ">About</p>
-        <p className="mr-6 cursor-pointer hover:text-teal-400">Articles</p>
-        <p className="mr-6 cursor-pointer hover:text-teal-400">Projects</p>
-        <p className="cursor-pointer hover:text-teal-400">Uses</p>
+        </a>
+        <a
+          href="/"
+          className={`mr-6 cursor-pointer hover:text-teal-400 ${
+            location.pathname.includes("/about") ? "text-teal-400" : ""
+          }`}
+        >
+          About
+        </a>
+        <a
+          href="/"
+          className={`mr-6 cursor-pointer hover:text-teal-400 ${
+            location.pathname.includes("/article") ? "text-teal-400" : ""
+          }`}
+        >
+          Articles
+        </a>
+        <a
+          href="/"
+          className={`mr-6 cursor-pointer hover:text-teal-400 ${
+            location.pathname.includes("/projects") ? "text-teal-400" : ""
+          }`}
+        >
+          Projects
+        </a>
+        <a
+          href="/"
+          className={`cursor-pointer hover:text-teal-400 ${
+            location.pathname.includes("/uses") ? "text-teal-400" : ""
+          }`}
+        >
+          Uses
+        </a>
       </div>
       <img
         src="/images/github.png"
