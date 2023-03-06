0f0476308594c4166a9205a89fe341372ed47bbb
Damien Dumontet <damien.dumontet@protonmail.com>
2023-03-06 18:23:31 -0300

    add nav and header

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 module.exports = {
-  plugins: ["jsx-a11y"],
-  extends: ["plugin:astro/recommended", "plugin:jsx-a11y/recommended"],
+  extends: ["eslint:recommended", "plugin:import/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
   overrides: [
     {
       files: ["*.astro"],
@@ -9,7 +16,23 @@ module.exports = {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+      extends: [
+        "plugin:astro/recommended",
+        "plugin:astro/jsx-a11y-recommended",
+      ],
       rules: {},
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "plugin:react-hooks/recommended",
+      ],
+    },
+    {
+      files: ["*.tsx"],
+      extends: ["plugin:react/recommended", "plugin:jsx-a11y/recommended"],
     },
   ],
 };
---
+++ src/components/ArticlesPreview.tsx
@@ -0,0 +1,5 @@
+import type React from "react";
+
+export default function ArticlePreview(): React.ReactElement {
+  return <h1>Nav</h1>;
+}
