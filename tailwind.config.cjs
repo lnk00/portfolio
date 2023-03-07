ddd93520a3002a1144e145df66d9c5fc5604c5c0
Damien Dumontet <damien.dumontet@protonmail.com>
2023-03-07 07:39:56 -0300

    add recent publication section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -7,5 +10,11 @@ module.exports = {
       sans: ["Poppins"],
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    plugin(function ({ addVariant }) {
+      addVariant("not-last", "&:not(:last-child)");
+    }),
+  ],
 };
